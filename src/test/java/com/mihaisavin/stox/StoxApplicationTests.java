55ac19ffde5a4b37c59afcb15b2b8a60adb0b763
Mihai Savin <savin.mihai@gmail.com>
2018-02-28 13:54:13 +0200

    Added Spring generated web app stub with merged .gitignore file.

---
+++ src/test/java/com/mihaisavin/stox/StoxApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mihaisavin.stox;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StoxApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
