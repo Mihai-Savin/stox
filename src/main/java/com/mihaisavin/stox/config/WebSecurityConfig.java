58fcee03aa14fa880ff357e7a4cf135bcc03eaa6
Mihai Savin <savin.mihai@gmail.com>
2018-03-11 17:04:27 +0200

    Configs.

--- src/main/java/com/mihaisavin/stox/config/WebSecurityConfig.java
+++ src/main/java/com/mihaisavin/stox/config/WebSecurityConfig.java
@@ -1,15 +1,20 @@
 package com.mihaisavin.stox.config;
 
+import com.mihaisavin.stox.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    UserService userService;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -30,5 +35,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         auth
                 .inMemoryAuthentication()
                 .withUser("mihai").password("").roles("USER");
+        auth.userDetailsService(userService);
     }
 }
